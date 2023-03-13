cd794f2ed47a7b850ba8092b95e1339e4368ae3b
BritanyRH2125 <61550366+BritanyRH2125@users.noreply.github.com>
2023-03-13 10:56:55 -0600

    Semana 3
    Ejercicio trabajado en clase (incompleto)

---
+++ Semana3/src/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[]args) {
+        Persona per = new Persona("Pedro",45,true);
+        Persona per2 = new Persona("Yindra",15,false);
+        System.out.println(per.toString());
+
+    }
+
+}
