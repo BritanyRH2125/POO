d63f4753cd2b321cb09826e94dbb1d1fc8bc098f
BritanyRH2125 <61550366+BritanyRH2125@users.noreply.github.com>
2023-03-16 08:59:08 -0600

    Lab1
    Lab1 ( en proceso)

---
+++ Lab1/src/Persona.java
@@ -0,0 +1,19 @@
+public class Persona {
+
+
+
+    private String nombre;
+    private int cedula;
+    private String genero;
+    private String adn;
+
+    public Persona(String nombre, int cedula, String genero, String adn) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.genero = genero;
+        this.adn = adn;
+    }
+
+}
+
+
